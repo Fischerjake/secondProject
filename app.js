5e4c3d3f44ff85ec949cdefc5da4f91aa72ebaf5
Fischerjake <fischerjake98@gmail.com>
2024-03-17 14:41:33 -0500

    singing game

--- app.js
+++ app.js
@@ -1,76 +1,13 @@
-let firstName = "Jake";
+var friends = ["Jack", "Jill", "John", "Job", "James"]
 
-const states = 50;
-
-var answer = 4 + 5;
-
-//Theses commments are for the documentation of the code
-
-function sayHello() {
-    alert("Hello World!")
-}
-
-sayHello()
-
-function checkAge(name, age) {
-    if (age < 21) {
-        alert("Sorry " + name + " you aren't old enough to view this page!")
+friends.forEach((item) => {
+    console.log(item + ":")
+    for (i = 99; i >= 1; i--) {
+        if (i == 1) {
+            console.log(`${i} line of code in the file, ${i} line of code; ${item} strikes one out, clears it all out, no more lines of code in the file`)
+        }
+        else {
+            console.log(`${i} lines of code in the file, ${i} lines of code; ${item} strikes one out, clears it all out, ${i - 1} lines of code in the file`)
+        }
     }
-}
-
-checkAge("Charles", 21)
-checkAge("Abby", 27)
-checkAge("James", 18)
-checkAge("John", 17)
-
-var favoriteVegetables = ["Lettuce", "Apple", "Corn"]
-
-favoriteVegetables.forEach((item) =>
-    console.log(item));
-
-var pet = {
-    "name": "Finley",
-    "breed": "orange-tabby"
-}
-
-console.log(pet.name);
-console.log(pet.breed);
-
-var names = [
-    {
-        "name": "John",
-        "age": 22
-    },
-    {
-        "name": "Jim",
-        "age": 27
-    },
-    {
-        "name": "Jill",
-        "age": 18
-    },
-    {
-        "name": "Jame",
-        "age": 12
-    },
-    {
-        "name": "Jack",
-        "age": 290
-    }
-
-]
-
-names.forEach((item) =>
-    checkAge(item.name, item.age));
-
-function getLength(word) {
-    var len = word.length
-    if (len % 2 == 0) {
-        console.log("The world is nice and even!")
-    }
-    else {
-        console.log("The world is an odd place!")
-    }
-}
-
-getLength("Hello World")
+})
